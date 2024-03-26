543037bbad936dc7ae89f3798f4d5a3d73ddd3c6
Samuel_Yang <sam.ee11@nycu.edu.tw>
2024-03-26 10:05:43 +0800

    push data

--- read_dat.py
+++ read_dat.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Open the CSV file
-with open('tri_blink_signal_100Hz_long.Dat', 'r') as file:
+with open('./data/tri_blink_signal_1Hz_2ms.Dat', 'r') as file:
+
     # Create a CSV reader object
     csv_reader = csv.reader(file)
     
@@ -14,7 +15,7 @@ with open('tri_blink_signal_100Hz_long.Dat', 'r') as file:
     sigma = []
     diffs = []
     diffs2 = []
-    diffs.append(0)
+    
     
     i=0
     ts = 0.002
@@ -27,8 +28,7 @@ with open('tri_blink_signal_100Hz_long.Dat', 'r') as file:
             x.append(float(row[1]))
             y.append(float(row[2]))
             sigma.append(float(row[3]))
-            if i>0:
-                diffs.append((sigma[i]-sigma[i-1])/ts)
+
             i+=1
 
     diffs = np.diff(sigma)
@@ -55,12 +55,14 @@ for i, diff in enumerate(diffs2):
         sigma_temp.append(sigma[i])
         time_temp.append(time[i])
 
-# time = time[0:100]
-# x = x[0:100]
-# y = y[0:100]
-# sigma = sigma[0:100]
-# diffs = diffs[0:100]
-# diffs2 = diffs2[0:100]
+start = 800
+end = 1200
+time = time[start:end]
+x = x[start:end]
+y = y[start:end]
+sigma = sigma[start:end]
+diffs = diffs[start:end]
+diffs2 = diffs2[start:end]
 
 # x = []
 # y = []
@@ -71,44 +73,44 @@ for i, diff in enumerate(diffs2):
 # time = time_temp.copy()
 # print(len(x), len(y), len(sigma))
 # Create subplots
-# fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(10, 8), sharex=True)
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, 1, figsize=(10, 8), sharex=True)
 
-# # Plot X data
-# ax1.plot(time, x, label='X', color='r')
-# ax1.set_ylabel('X Value')
-# ax1.set_title('Data Plots')
-# ax1.grid(True)
-# ax1.legend()
+# Plot X data
+ax1.plot(time, x, label='X', color='r')
+ax1.set_ylabel('X Value')
+ax1.set_title('Data Plots')
+ax1.grid(True)
+ax1.legend()
 
-# # Plot Y data
-# ax2.plot(time, y, label='Y', color='g')
-# ax2.set_ylabel('Y Value')
-# ax2.grid(True)
-# ax2.legend()
+# Plot Y data
+ax2.plot(time, y, label='Y', color='g')
+ax2.set_ylabel('Y Value')
+ax2.grid(True)
+ax2.legend()
 
-# # Plot Sigma data
-# ax3.plot(time, sigma, label='Sigma', color='b')
-# ax3.set_xlabel('Time')
-# ax3.set_ylabel('Sigma Value')
-# ax3.grid(True)
-# ax3.legend()
+# Plot Sigma data
+ax3.plot(time, sigma, label='Sigma', color='b')
+ax3.set_xlabel('Time')
+ax3.set_ylabel('Sigma Value')
+ax3.grid(True)
+ax3.legend()
 
-# # Plot diff data
-# ax4.plot(time, diffs, label='Sigma', color='k')
-# ax4.set_xlabel('Time')
-# ax4.set_ylabel('diff Value')
-# ax4.grid(True)
-# ax4.legend()
+# Plot diff data
+ax4.plot(time, diffs, label='Sigma', color='k')
+ax4.set_xlabel('Time')
+ax4.set_ylabel('diff Value')
+ax4.grid(True)
+ax4.legend()
 
-# # Plot diff data
-# ax5.plot(time, diffs2, label='Sigma', color='k')
-# ax5.set_xlabel('Time')
-# ax5.set_ylabel('diff2 Value')
-# ax5.grid(True)
-# ax5.legend()
+# Plot diff data
+ax5.plot(time, diffs2, label='Sigma', color='k')
+ax5.set_xlabel('Time')
+ax5.set_ylabel('diff2 Value')
+ax5.grid(True)
+ax5.legend()
 
-# plt.tight_layout()
-# # plt.show()
+plt.tight_layout()
+# plt.show()
 
 
 plt.figure(figsize=(8, 6))
@@ -116,6 +118,8 @@ scatter = plt.scatter(x, y, marker='o', label='Data', s=0.5,c=sigma, cmap='virid
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('X vs Y Data')
+plt.xlim([-2.25, 2.25])
+plt.ylim([-2.25, 2.25])
 plt.grid(True)
 plt.legend()
 
